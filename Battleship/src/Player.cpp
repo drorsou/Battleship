07a02b8d7585baf092894e5938ec5e9b50a52a6c
NadavKeren <nadav.keren@gmail.com>
2017-04-10 15:21:10 +0300

    Changed the player constructor, changed @ to MISS_SYM, * to HIT_SYM

--- Battleship/src/Player.cpp
+++ Battleship/src/Player.cpp
@@ -1,7 +1,8 @@
 #include "Player.h"
 
-Player::Player(Ship ships[SHIPS_PER_PLAYER]) : ships{*ships}{
-	
+Player::Player(Ship ships[SHIPS_PER_PLAYER]){
+	for (int i = 0; i < SHIPS_PER_PLAYER; i++)
+		this->ships[i] = ships[i];
 };
 
 void Player::setBoard(const char** board, int numRows, int numCols) {
@@ -17,9 +18,9 @@ pair<int, int> Player::attack() {
 
 void Player::notifyOnAttackResult(int player, int row, int col, AttackResult result) {
 	if (result == AttackResult::Miss)
-		player_board[row][col] = '@';
+		player_board[row][col] = MISS_SYM;
 	else if (result == AttackResult::Hit)
-		player_board[row][col] = '*';
+		player_board[row][col] = HIT_SYM;
 	else if (result == AttackResult::Sink)
-		player_board[row][col] = '*';
+		player_board[row][col] = HIT_SYM;
 };
