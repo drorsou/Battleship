448a9aff9e8c26ed0f793238c59f5082bbdadb1e
Dror <drorsu@gmail.com>
2017-04-17 13:02:26 +0300

    Fixed a print problem

--- Battleship/src/Player.cpp
+++ Battleship/src/Player.cpp
@@ -8,18 +8,17 @@ Player::Player(int player_number) : player_number(player_number) {
 
 
 void Player::setBoard(const char** board, int numRows, int numCols) {
-	for (int i = 0; i < numRows; i++) {
-		for (int j = 0; j < numCols; j++)
+	for (int i = 0; i < numRows; i++)
+		for (int j = 0; j < numCols; j++) {
 			player_board[i][j] = board[i][j];
-	}
-
-	if (player_number == 0)
-	{
-		// Change the player_board, ships accordingly // TODO
-	}
-	else if (player_number == 1)
-	{
-		// Change the player_board, ships accordingly // TODO
+			if (player_number == 0) {
+				if (board[i][j] == BBOAT || board[i][j] == BCRUISER || board[i][j] == BSUBMARINE || board[i][j] == BDESTROYER)
+					player_board[i][j] = BLANK;
+			}
+			if (player_number == 1) {
+				if (board[i][j] == ABOAT || board[i][j] == ACRUISER || board[i][j] == ASUBMARINE || board[i][j] == ADESTROYER)
+					player_board[i][j] = BLANK;
+			}
 	}
 };
 
