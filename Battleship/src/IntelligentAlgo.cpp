fe8d3478657b105f91b1d2f823351c02eaf66038
NadavKeren <nadav.keren@gmail.com>
2017-05-06 18:10:58 +0300

    Added notifyOnAttackResult to IntelligentAlgo. Without testing yet.

--- Battleship/src/IntelligentAlgo.cpp
+++ Battleship/src/IntelligentAlgo.cpp
@@ -1,5 +1,156 @@
 #include "IntelligentAlgo.h"
 
+
+void IntelligentAlgo::addTwo()
+{
+	if (nextAttack.second >= numOfCols - 2)
+	{
+		if (nextAttack.first < numOfRows - 1)
+			numberOfRuns++;
+		nextAttack.first = (nextAttack.first + 1) % numOfRows;
+	}
+	nextAttack.second = (nextAttack.second + 2) % numOfCols;
+}
+
+void IntelligentAlgo::markLeft(int row, int col)
+{
+	if (col > 0)
+		shadow_board[row][col - 1] = DontAttack;
+}
+
+void IntelligentAlgo::markRight(int row, int col)
+{
+	if (col < numOfCols)
+		shadow_board[row][col + 1] = DontAttack;
+}
+
+void IntelligentAlgo::markUp(int row, int col)
+{
+	if (row > 0)
+		shadow_board[row - 1][col] = DontAttack;
+}
+
+void IntelligentAlgo::markDown(int row, int col)
+{
+	if (row < numOfRows)
+		shadow_board[row + 1][col] = DontAttack;
+}
+
+void IntelligentAlgo::addAttackLeft(int row, int col)
+{
+	if (col > 0 && shadow_board[row][col - 1] == Attack)
+		possibleAttacks.emplace(row, col - 1, Left);
+}
+
+void IntelligentAlgo::addAttackRight(int row, int col)
+{
+	if (col < numOfCols - 1 && shadow_board[row][col + 1] == Attack)
+		possibleAttacks.emplace(row, col + 1, Right);
+}
+
+void IntelligentAlgo::addAttackUp(int row, int col)
+{
+	if (row > 0 && shadow_board[row - 1][col] == Attack)
+		possibleAttacks.emplace(row - 1, col, Up);
+}
+
+void IntelligentAlgo::addAttackDown(int row, int col)
+{
+	if (row < numOfRows - 1 && shadow_board[row + 1][col] == Attack)
+		possibleAttacks.emplace(row + 1, col, Down);
+}
+
+void IntelligentAlgo::markSink(int row, int col, direction dir)
+{
+	switch (dir)
+	{
+	case Up:
+		markUp(row, col);
+		markLeft(row, col);
+		markRight(row, col);
+		break;
+	case Down:
+		markDown(row, col);
+		markLeft(row, col);
+		markRight(row, col);
+		break;
+	case Right:
+		markDown(row, col);
+		markUp(row, col);
+		markRight(row, col);
+		break;
+	case Left:
+		markDown(row, col);
+		markLeft(row, col);
+		markUp(row, col);
+		break;
+	case None:
+		markUp(row, col);
+		markDown(row, col);
+		markLeft(row, col);
+		markRight(row, col);
+		break;
+	}
+}
+
+void IntelligentAlgo::markHit(int row, int col, direction dir)
+{
+	switch(dir)
+	{
+	case Up:
+		markLeft(row, col);
+		markRight(row, col);
+		markLeft(row - 1, col);
+		markRight(row - 1, col);
+		break;
+	case Down:
+		markLeft(row, col);
+		markRight(row, col);
+		markLeft(row + 1, col);
+		markRight(row + 1, col);
+		break;
+	case Left:
+		markLeft(row, col);
+		markRight(row, col);
+		markLeft(row, col + 1);
+		markRight(row, col + 1);
+		break;
+	case Right:
+		markLeft(row, col);
+		markRight(row, col);
+		markLeft(row, col - 1);
+		markRight(row, col - 1);
+		break;
+	case None:
+		break;
+	}
+}
+
+void IntelligentAlgo::addAttacks(int row, int col, direction dir)
+{
+	switch (dir)
+	{
+	case Up:
+		addAttackUp(row, col);
+		break;
+	case Down:
+		addAttackDown(row, col);
+		break;
+	case Left:
+		addAttackLeft(row, col);
+		break;
+	case Right:
+		addAttackRight(row, col);
+		break;
+	case None:
+		addAttackUp(row, col);
+		addAttackDown(row, col);
+		addAttackLeft(row, col);
+		addAttackRight(row, col);
+		break;
+	}
+}
+
 void IntelligentAlgo::setBoard(int color, const char** board, int numRows, int numCols) {
 	player_number = color;
 	for (int row = 0; row < numRows; row++)
@@ -36,29 +187,35 @@ bool IntelligentAlgo::init(const std::string& path)
 
 std::pair<int, int> IntelligentAlgo::attack()
 {
-	std::pair<int, int> res;
+	std::tuple<int,int,direction> temp;
 	if(!possibleAttacks.empty())
 	{
-		res = possibleAttacks.top();
-		possibleAttacks.pop();
-		return res;
+		do
+		{
+			temp = possibleAttacks.top();
+			possibleAttacks.pop();
+		} while (shadow_board[std::get<0>(temp)][std::get<1>(temp)] != Attack);
+		lastFired = temp;
+		return make_pair(std::get<0>(temp), std::get<1>(temp));
 	}	
-	while(nextAttack.first < numOfRows - 1 && nextAttack.second < numOfCols - 1) // not suppose to happen
+	while(numberOfRuns < 2) // not suppose to happen
 	{
 		if (shadow_board[nextAttack.first][nextAttack.second] == Attack)
 		{
-			res = nextAttack;
-			addOne();
+			std::pair<int,int> res = nextAttack;			
+			addTwo();
+			lastFired = make_tuple(res.first, res.second, None);
 			return res;
 		}
 		// If marked Don't Attack or Attacked, keep looking for a target
-		addOne();
+		addTwo();
 	}
 	/*
 	 *  We get here only if there are no more targets (and the last target is 10,10)
 	 *  We should get here only once - and win in this turn 
 	 *  (otherwise we have passed all the possible tiles and we haven't sank all of the enemy ships)
 	*/
+	lastFired = make_tuple(nextAttack.first, nextAttack.second, None);
 	return nextAttack;
 }
 
@@ -66,29 +223,27 @@ void IntelligentAlgo::notifyOnAttackResult(int player, int row, int col, AttackR
 {
 	if(result == AttackResult::Miss)
 	{
-		board[row][col] = MISS_SYM;		
+		board[row][col] = MISS_SYM;	
 	}
 	else
-	{
-		if (!checkColor(row, col))
+	{			
+		if(player == player_number)
 		{
-			if(row > 0 && shadow_board[row - 1][col] == tileMarks::Attack)
+			if (result == AttackResult::Sink)
 			{
-				possibleAttacks.emplace(row - 1, col);
+				markSink(row, col, std::get<2>(lastFired));
 			}
-			if(col > 0 && shadow_board[row][col - 1] == tileMarks::Attack)
+			else
 			{
-				possibleAttacks.emplace(row, col - 1);
-			}
-			if (row < numOfRows - 1 && shadow_board[row + 1][col] == tileMarks::Attack)
-			{
-				possibleAttacks.emplace(row + 1, col);
-			}
-			if (col < numOfCols && shadow_board[row][col + 1] == tileMarks::Attack)
-			{
-				possibleAttacks.emplace(row, col + 1);
+				markHit(row, col, std::get<2>(lastFired));
+				addAttacks(row, col, std::get<2>(lastFired));
 			}
 		}
+		else if (board[row][col] == BLANK)
+		{
+			addAttacks(row, col, None);
+		}
+		board[row][col] = HIT_SYM;
 	}
 	shadow_board[row][col] = tileMarks::Attacked;
 }
--- Battleship/src/IntelligentAlgo.h
+++ Battleship/src/IntelligentAlgo.h
@@ -2,8 +2,10 @@
 #include "IBattleshipGameAlgo.h"
 #include "Ship.h"
 #include <stack>
+#include <tuple>
 #include "Board.h"
 enum tileMarks { DontAttack, Attack, Attacked };
+enum direction { Left, Right, Up, Down, None };
 class IntelligentAlgo : public IBattleshipGameAlgo
 {
 	int player_number;
@@ -11,9 +13,10 @@ class IntelligentAlgo : public IBattleshipGameAlgo
 	int numOfRows;
 	int numOfCols;
 	tileMarks ** shadow_board;	
-	std::stack<pair<int, int>> possibleAttacks;	
+	std::stack<std::tuple<int, int, direction>> possibleAttacks;	
 	std::pair<int, int> nextAttack;
-
+	std::tuple<int,int,direction> lastFired;
+	int numberOfRuns;
 	/*
 	 * Pre: nextAttack is a valid coordinate.
 	 * Invariant: All tiles up to nextAttack (from top left)
@@ -21,32 +24,47 @@ class IntelligentAlgo : public IBattleshipGameAlgo
 	 * Post: If the tile is smaller than 10,10 move it one tile
 	 *		 to the right or to the start of the next row.
 	 */
-	void addOne()
+	void addTwo();
+
+	void markLeft(int row, int col);
+	void markRight(int row, int col);
+	void markUp(int row, int col);
+	void markDown(int row, int col);
+	void addAttackLeft(int row, int col);
+	void addAttackRight(int row, int col);
+	void addAttackUp(int row, int col);
+	void addAttackDown(int row, int col);
+
+	void markSink(int row, int col, direction dir);
+
+	void markHit(int row, int col, direction dir);
+
+	void addAttacks(int row, int col, direction dir);
+	
+
+	/*direction adjacentShot(int row, int col) const
 	{
-		if (nextAttack.second < numOfCols - 1)
-			nextAttack.second++;
-		else if (nextAttack.first < numOfRows - 1)
-		{
-			nextAttack.first++;
-			nextAttack.second = 0;
-		}
-	}
-	bool checkColor(int row, int col) const
-	{
-		return (this->board[row][col] >= 'A' && this->board[row][col] <= 'Z') ? 0 == player_number : 1 == player_number;
-	}
+		if (std::get<0>(lastFired) - row >= -1 && std::get<0>(lastFired) - row <= 1 && std::get<1>(lastFired) == col)
+			return Vertical;
+		if (std::get<0>(lastFired) == row && std::get<1>(lastFired) - col >= -1 && std::get<1>(lastFired) - col <= 1)
+			return Horizontal;
+		return None;
+	}*/
 public:
 
 
 	IntelligentAlgo(int player_number, int num_of_rows, int num_of_cols)
 		: player_number(player_number),
 		  numOfRows(num_of_rows),
-		  numOfCols(num_of_cols)
+		  numOfCols(num_of_cols),
+		  numberOfRuns(0)
 	{
 		shadow_board = new tileMarks*[numOfRows];
+		board = new char*[numOfRows];
 		for (int i = 0; i < numOfRows; i++)
 		{
 			shadow_board[i] = new tileMarks[numOfCols];
+			board[i] = new char[numOfCols];
 			for (int j = 0; j < numOfCols; j++)
 			{
 				shadow_board[i][j] = tileMarks::Attack;
@@ -56,6 +74,11 @@ public:
 	}
 	~IntelligentAlgo()
 	{
+		for (int i = 0; i < numOfRows; i++)
+		{
+			delete[] board[i];
+			delete[] shadow_board[i];
+		}
 		delete[] board;
 		delete[] shadow_board;
 	}
