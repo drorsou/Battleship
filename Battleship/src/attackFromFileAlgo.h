209c0a4694785dc0a3b2cd09cf7d27e8f336f8a1
NadavKeren <nadav.keren@gmail.com>
2017-05-07 18:57:23 +0300

    Fixed an issues with the constructor in attackFromFileAlgo

--- Battleship/src/attackFromFileAlgo.h
+++ Battleship/src/attackFromFileAlgo.h
@@ -12,7 +12,8 @@ class attackFromFileAlgo: public IBattleshipGameAlgo {
 	int player_number; // 0 for A and 1 for B
 
 public:
-	explicit attackFromFileAlgo() {};
+	explicit attackFromFileAlgo() {}
+	explicit attackFromFileAlgo(int player) : player_number(player) {};
 
 	void setBoard(int player, const char** board, int numRows, int numCols) override; // called once to notify player on his board
 	bool init(const std::string& path) override; // called once to allow init from files if needed returns whether the init succeeded or failed
